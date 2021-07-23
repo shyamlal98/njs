356eb96c2e4cb986a601f42cca903f6b8e63d98a
shyamlal98 <shyaml.cse16@gmail.com>
2021-07-23 19:44:51 +0530

    api with middleware and logger

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(express.urlencoded({extended:false}))
 //parse json data
 app.use(express.json())
 
-
 app.get('/api/people',(req,res)=>{
     console.log(req.body);
     res.status(200).json({success:true,people:people});
