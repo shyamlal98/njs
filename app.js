54ff3991d1d3d02c0f78b37439785b07d126b68b
shyamlal98 <shyaml.cse16@gmail.com>
2021-07-23 19:40:59 +0530

    api with middleware and logger

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ app.use(express.static('./method-public'))
 // parse form data
 app.use(express.urlencoded({extended:false}))
 
-//
+//parse json data
 app.use(express.json())
+
+
 app.get('/api/people',(req,res)=>{
     console.log(req.body);
     res.status(200).json({success:true,people:people});
